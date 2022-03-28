8cfec307c8c6a550d955d2b20ac9ce6bb822f7c0
MohammadRafi <m.rafi@veroke.com>
2022-03-28 17:31:34 +0500

    Initial Commit

---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by Andpercent on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
